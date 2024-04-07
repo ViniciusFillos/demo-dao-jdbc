99676c66c5ffbd5bb2caee2d67faafc0e88762f2
Vinicius Fillos <vinifillos@gmail.com>
2024-04-07 09:37:52 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDateTime;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.0, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
